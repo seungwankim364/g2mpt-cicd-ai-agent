d5c8f190f9c36882d0670facbc2ffcddf60b1595
seungwankim364 <seungwankim364@github.com>
2026-06-27 00:45:24 +0900

    Allow analysis orchestrator to verify Athena output bucket

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -35,6 +35,7 @@ resource "aws_iam_role_policy" "analysis_orchestrator" {
       {
         Effect = "Allow"
         Action = [
+          "s3:GetBucketLocation",
           "s3:GetObject",
           "s3:PutObject",
           "s3:ListBucket"
